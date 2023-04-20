b84e5b3435782d62d7fd2c0c5ecdf92a03e521ee
DESKTOP-8GFJPDK\vytau <vytautas.leveris@gmail.com>
2023-04-20 10:48:33 +0300

    Blazor and WPF template improvements.

---
+++ SubtitleDubberApp/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SubtitleDubberApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+        }
+    }
+
+    // Workaround for compiler error "error MC3050: Cannot find the type 'local:Main'"
+    // It seems that, although WPF's design-time build can see Razor components, its runtime build cannot.
+    public partial class Main { }
+
+    // Helpful guide on WCF: https://docs.microsoft.com/en-us/aspnet/core/blazor/hybrid/tutorials/wpf?view=aspnetcore-6.0
+}
